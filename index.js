a12b77a50c53273e002b522afbf7e1cfa4fddb0f
Eren Kulahlioglu <erkulahlioglu@gmail.com>
2020-06-13 23:39:33 +0100

    port added

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.static('public'));
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 
 console.log('Running at Port 3000'); 
